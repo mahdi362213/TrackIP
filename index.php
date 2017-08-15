4d20bdd2f6e6814ec783bee15a11457bdc0054ae
virtualdemon <virtual.demon.message@gmail.com>
2017-08-15 15:28:56 +0430

    changes on the index.php are started...

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>This page is SAVE your ip!</title>
+</head>
+<body>
+
+<!-- webpage content goes here in the body -->
+	<div id="page">
+		<div id="logo">
+			<h1>My First Website</h1>
+		</div>
+		<div id="nav">
+			<ul>
+				<li><a href="#/home.html">Home</a></li>
+				<li><a href="#/about.html">About</a></li>
+				<li><a href="#/contact.html">Contact</a></li>
+			</ul>
+		</div>
+		<div id="content">
+			<h2>Home</h2>
+			<p>
+				This is my first webpage! I was able to code all the HTML and CSS in order to make it. Watch out world of web design here I come!
+			</p>
+			<p>
+				I can use my skills here to create websites for my business, my friends and family, my C.V, blog or articles. As well as any games or more experiment stuff (which is what the web is really all about).
+			</p>
+		</div>
+		<div id="footer">
+			<p>
+				Webpage made by <a href="http://shotopia.ml" target="_blank">VirtualDemon</a>
+			</p>
+		</div>
+	</div>
+</body>
+</html>
